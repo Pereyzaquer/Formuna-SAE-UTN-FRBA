b7eeaca4a8b8614fc07c2762c2f9d83a771b0afc
Agustin Giovanazzi <agiovanazzi@frba.utn.edu.ar>
2025-12-29 07:40:21 -0300

    Se agrega proyecto basico para ESP32 C3 super mini, para los sensores de RPM de las ruedas

---
+++ Code/RPM_Sensor/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(8, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(1000);
+  digitalWrite(8,HIGH);
+  delay(1000);
+  digitalWrite(8,LOW);
+}
